a3cc0e5f27688278d0ab1e84c199e8d0e0c9f9d3
aminhp93 <minhpn.org.ec@gmail.com>
2021-12-18 14:51:46 +0700

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom'
 
 import './styles/index.less';
 
@@ -10,7 +11,10 @@ import store from './store/store';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
+
     </Provider>
 
   </React.StrictMode>,
