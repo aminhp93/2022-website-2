04ddb9039c63e5374f471bd48062bcbbef16824e
aminhp93 <minhpn.org.ec@gmail.com>
2021-12-18 15:06:39 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -11,27 +11,25 @@ import ReactGridRoot from './pages/ReactGridRoot/ReactGridRoot'
 function App() {
   return (
     <div className="App">
-      <Router>
-        <div style={{ height: "100%" }}>
-          <Switch>
-            <Route path="/nhi">
-              <Nhi />
-            </Route>
-            <Route path="/">
-              <ReactGridRoot />
-            </Route>
-            <Route path="/">
-              <nav>
-                <ul>
-                  <li>
-                    <Link to="/">Home</Link>
-                  </li>
-                </ul>
-              </nav>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <div style={{ height: "100%" }}>
+        <Switch>
+          <Route path="/nhi">
+            <Nhi />
+          </Route>
+          <Route path="/">
+            <ReactGridRoot />
+          </Route>
+          <Route path="/">
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+              </ul>
+            </nav>
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
