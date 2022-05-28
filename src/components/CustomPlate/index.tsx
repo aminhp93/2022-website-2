6c634aa4699d644947112c8fe14b699d506cb671
aminhp93 <minhpn.org.ec@gmail.com>
2022-05-28 17:24:50 +0700

    Update

--- src/components/CustomPlate/index.tsx
+++ src/components/CustomPlate/index.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import {
     Plate,
     ELEMENT_PARAGRAPH,
@@ -94,7 +95,60 @@ interface IProps {
     // children?: any;
 }
 
+
+function b64toBlob(b64Data, contentType?, sliceSize?) {
+    contentType = contentType || '';
+    sliceSize = sliceSize || 512;
+
+    var byteCharacters = atob(b64Data);
+    var byteArrays = [];
+
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+        var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+        var byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        var byteArray = new Uint8Array(byteNumbers);
+
+        byteArrays.push(byteArray);
+    }
+
+    var blob = new Blob(byteArrays, { type: contentType });
+    return blob;
+}
+
 export default function CustomPlate(props: IProps) {
+
+    const cbUploadImage = async (data) => {
+        // Split the base64 string in data and contentType
+        const block = data.split(";");
+        // Get the content type of the image
+        const contentType = block[0].split(":")[1];// In this case "image/gif"
+        // get the real base64 content of the file
+        const realData = block[1].split(",")[1];// In this case "R0lGODlhPQBEAPeoAJosM...."
+        // Convert it to a blob to upload
+        const blob = b64toBlob(realData, contentType);
+
+        const dataRequest = new FormData()
+        dataRequest.append('auth_token', '970165a01aa329064b5154c75c6cbc99183ddb8c')
+        dataRequest.append('source', blob)
+        dataRequest.append('type', 'file')
+        dataRequest.append('action', 'upload')
+        dataRequest.append('timestamp', JSON.stringify(new Date().getTime()))
+
+        const res = await axios({
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+            url: 'https://imgbb.com/json',
+            method: 'POST',
+            data: dataRequest
+        })
+        return res.data.image.display_url
+    }
 
     const BallonToolbarMarks = () => {
         const editor = usePlateEditorRef();
@@ -146,7 +200,11 @@ export default function CustomPlate(props: IProps) {
         createBlockquotePlugin(),
         createTodoListPlugin(),
         createHeadingPlugin(),
-        createImagePlugin(),
+        createImagePlugin({
+            options: {
+                uploadImage: cbUploadImage
+            } as any
+        }),
         createHorizontalRulePlugin(),
         createLineHeightPlugin(CONFIG.lineHeight),
         createLinkPlugin(),
